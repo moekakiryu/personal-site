76ace33df079e2653354e0bb471c737be24b940c
Moeka Kiryu <moekakiryutaku@gmail.com>
2025-02-23 20:53:01 +1100

    Update deployment script to be more robust

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,4 +1,30 @@
 # This is very basic and flaky, this should be updated with extra checks and fallbacks in the future
-git pull
+
+# Verify this script is being run in the correct directory
+# Note: We assume that finding an django entry is enough verification for this
+if [ ! -f manage.py ]; then
+  echo "Unable to find Django project. Please ensure this script is run from the project root."
+  exit 1;
+fi
+
+# Activate python virtual environment
+if [ ! -f .venv/bin/activate ]; then
+  echo "Python virtual environment not initialized"
+  exit 1
+fi
+source .venv/bin/activate
+
+echo -e "\n\n-- Installing new requirements --"
+python3 -m pip install -r requiremetns.txt
+
+echo -e "\n\n-- Collecting Django updates --"
 python3 manage.py collectstatic
+
+echo -e "\n\n-- Collecting Django updates --"
+python3 manage.py migrate
+
+# Deactivate python virtual environment since all python operations have been completed
+deactivate
+
+echo -e "\n\n-- Restarting gunicorn service --"
 sudo systemctl restart gunicorn
