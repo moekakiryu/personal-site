08f2280be7cc07d5dafb15d3a2873302611866cd
Moeka Kiryu <moekakiryutaku@gmail.com>
2025-02-26 02:34:55 +1100

    Rename home view to 'home'

--- apps/root/urls.py
+++ apps/root/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name="root.home"),
+    path('', views.home, name="root.home"),
     path("sandbox/", views.sandbox, name="root.sandbox"),
 ]
--- apps/root/views.py
+++ apps/root/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def index(request):
+def home(request):
   return render(request, 'root/home/index.html')
 
 # Create your views here.
