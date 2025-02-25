48533b21714531f97d55c7fb5a44faa518ae3bbd
Moeka Kiryu <moekakiryutaku@gmail.com>
2025-02-26 02:40:01 +1100

    Render project grid with mock data

--- apps/root/views.py
+++ apps/root/views.py
@@ -2,7 +2,28 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-  return render(request, 'root/home/index.html')
+  mock_projects = [
+    {
+      "name": "Project",
+      "description": "Lorem ipsum"
+    },
+    {
+      "name": "Project",
+      "description": "Lorem ipsum"
+    },
+    {
+      "name": "Project",
+      "description": "Lorem ipsum"
+    },
+    {
+      "name": "Project",
+      "description": "Lorem ipsum"
+    },
+  ]
+
+  return render(request, 'root/home/index.html', {
+    'projects': mock_projects
+  })
 
 # Create your views here.
 def sandbox(request):
