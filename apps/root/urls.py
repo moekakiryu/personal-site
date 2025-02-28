aa799850cfaaba70bd2b6c82aa6e2a1b0ab9b4e7
Moeka Kiryu <moekakiryutaku@gmail.com>
2025-02-28 22:08:31 +1100

    Add blog template

--- apps/root/urls.py
+++ apps/root/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name="root.home"),
+    path('blog/<uuid:article_id>/', views.blog, name="root.blog.entry"),
     path("sandbox/", views.sandbox, name="root.sandbox"),
 ]
--- apps/root/views.py
+++ apps/root/views.py
@@ -1,15 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-from apps.root.models import Project
+from apps.root.models import BlogEntry, Project
 
 def home(request):
   projects = Project.objects.all()
+  articles = BlogEntry.objects.all()
 
   return render(request, 'root/home/index.html', {
-    'projects': projects
+    'projects': projects,
+    'articles': articles,
   })
 
-# Create your views here.
+def blog(request, article_id):
+  article = BlogEntry.objects.get(slug=article_id)
+
+  return render(request, 'root/blog/index.html', {
+    'article': article
+  })
+
 def sandbox(request):
   return render(request, 'root/sandbox/index.html')
