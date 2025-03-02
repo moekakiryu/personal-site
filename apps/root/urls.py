9e600ce7d9b0e3aa3d5542f7306f348ac7c6e7bd
Moeka Kiryu <moekakiryutaku@gmail.com>
2025-03-03 02:42:40 +1100

    Add basic templates for Article and Project listing pages

--- apps/root/urls.py
+++ apps/root/urls.py
@@ -4,6 +4,8 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name="root.home"),
+    path('projects/', views.projects, name="root.projects"),
+    path('articles/', views.articles, name="root.articles"),
     path('articles/<uuid:article_id>/', views.article, name="root.article"),
     path("sandbox/", views.sandbox, name="root.sandbox"),
 ]
--- apps/root/views.py
+++ apps/root/views.py
@@ -3,14 +3,33 @@ from django.http import HttpResponse
 
 from apps.root.models import Article, Project
 
+
 def home(request):
-  projects = Project.objects.all()
-  articles = Article.objects.all()
+  article_count = 3
+  project_count = 4
+
+  projects = Project.objects.all().reverse()
+  articles = Article.objects.all().order_by('-created')
 
   return render(request, 'root/home/index.html', {
-    'projects': projects,
-    'articles': articles,
+    'projects': {
+      'list': projects[:project_count],
+      'has_more': projects.count() > project_count
+    },
+    'articles': {
+      'list': articles[:article_count],
+      'has_more': articles.count() > article_count
+    },
   })
+
+
+def articles(request):
+  articles = Article.objects.all().order_by('-created')
+
+  return render(request, 'root/articles/index.html', {
+    'articles': articles
+  })
+
 
 def article(request, article_id):
   article = Article.objects.get(slug=article_id)
@@ -18,6 +37,15 @@ def article(request, article_id):
   return render(request, 'root/article/index.html', {
     'article': article
   })
+
+
+def projects(request):
+  projects = Project.objects.all().reverse()
+
+  return render(request, 'root/projects/index.html', {
+    'projects': projects
+  })
+
 
 def sandbox(request):
   return render(request, 'root/sandbox/index.html')
