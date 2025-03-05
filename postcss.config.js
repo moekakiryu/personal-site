a2650500edb155b9235fcd1ce6a9b19ae44290ad
Moeka Kiryu <moekakiryutaku@gmail.com>
2025-03-06 00:41:08 +1100

    Replace hard-coded media queries with variables

--- postcss.config.js
+++ postcss.config.js
@@ -2,6 +2,7 @@ module.exports = {
   plugins: [
     require('postcss-import'),
     require('postcss-nested'),
+    require('postcss-custom-media'),
     require('postcss-nested-props'),
     require('autoprefixer'),
     require('cssnano'),
