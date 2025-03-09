963cc3200fbda27a5b42da9ef3eea932f23b61ae
Moeka Kiryu <moekakiryutaku@gmail.com>
2025-03-10 00:33:08 +1100

    Resolve fast session timeout issue
    The session timeout cookie was being set using the wrong time zone, resulting
    in very short timeout periods. By setting the timezone correctly, the session
    length should now work as configured.

--- core/settings.py
+++ core/settings.py
@@ -14,8 +14,8 @@ DEBUG = CURRENT_ENVIRONMENT == Env.DEV
 
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
-LANGUAGE_CODE = 'en-us'
-TIME_ZONE = 'UTC'
+LANGUAGE_CODE = 'en-au'
+TIME_ZONE = 'Australia/Melbourne'
 USE_I18N = True
 USE_TZ = True
 
