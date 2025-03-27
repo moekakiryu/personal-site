89619d83942ae68356a24e4120a29cefb0df0f94
Moeka Kiryu <moekakiryutaku@gmail.com>
2025-03-28 04:10:26 +1100

    Remove compiled css from git tracking
    The performance issue on the hosting server that necessitated local builds
    should be resolved now

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -32,10 +32,12 @@ fi
 source .venv/bin/activate
 
 echo -e "\n\n-- Installing new requirements --"
-# Disable yarn install until server-side CSS builds are fixed
-# yarn install --immutable
 
+yarn install --immutable
 python3 -m pip install -r requirements.txt
+
+echo -e "\n\n-- Compiling Static Files --"
+yarn build:css
 
 echo -e "\n\n-- Collecting Django Static Files --"
 python3 manage.py collectstatic --no-input
