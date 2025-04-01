963d8ebbde11e2fee4f318ae268239980c7148a8
Moeka Kiryu <moekakiryutaku@gmail.com>
2025-04-02 05:14:58 +1100

    Add scss source maps in development environment

--- apps/root/postcss.config.js
+++ apps/root/postcss.config.js
@@ -1,3 +1,12 @@
+require('dotenv').config({
+  path: [
+    '../../.env',
+    '../../.env.local',
+    '../../.env.dev',
+    '../../.env.prod',
+  ]
+})
+
 const generated_header = `/*
 
   THIS FILE IS AUTO-GENERATED - DO NOT EDIT IT DIRECTLY.
@@ -7,7 +16,7 @@ const generated_header = `/*
 
 module.exports = {
   parser: 'postcss-scss',
-  map: false, // [1]
+  map: process.env.DJANGO_ENV === 'dev',
   plugins: [
     require('postcss-import'),
     require('postcss-nested'),
@@ -20,13 +29,3 @@ module.exports = {
     }),
   ]
 }
-
-/**
- *  1. This project currently has to build all static files locally (instead of
- *     on the remove machine), as a result it would be difficult to 
- *     differentiate dev and production builds without multiple commands, which
- *     introduces its own set of risks.
- * 
- *     For now we will disable sourcemaps entirely, but should reassess once the
- *     remote build issues are resolved.
- */
