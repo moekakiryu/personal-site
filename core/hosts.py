e2c4ba70bf3b60fc686e0dd61f93bc76c56a8372
Moeka Kiryu <moekakiryutaku@gmail.com>
2025-05-12 02:54:53 +1000

    Fix path error in django-hosts config

--- core/hosts.py
+++ core/hosts.py
@@ -1,6 +1,6 @@
 from django_hosts import patterns, host
 
-host_patterns = patterns('path.to',
-    host(r'', 'apps.root.urls', name='root'),
-    host(r'metro', 'apps.metro.urls', name='metro'),
+host_patterns = patterns('apps',
+    host(r'', 'root.urls', name='root'),
+    host(r'metro', 'metro.urls', name='metro'),
 )
